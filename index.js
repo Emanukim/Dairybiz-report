a99ace4502c5ccbb3b8eaa3606db955f92217354
Emmanuel Kimweli <emmanue.kimweli@student.moringaschool.com>
2022-07-19 16:31:33 +0300

    add js to

---
+++ index.js
@@ -0,0 +1,10 @@
+function totalMilk()
+{
+    num1 = document.getElementById("shA").value;
+    num2 = document.getElementById("shB").value;
+    num3 = document.getElementById("shC").value;
+    num4 = document.getElementById("shD").value;
+    num5 = document.getElementById("shE").value;
+    num6 = document.getElementById("shF").value;
+    document.getElementById("result").innerHTML=num1+num2+num3+num4+num5+num6;
+}
